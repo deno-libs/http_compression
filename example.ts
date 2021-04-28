84fc519fdccfd8f24b39b16a4edc75c9dec79ace
v1rtl <pilll.pl22@gmail.com>
2021-04-28 21:05:14 +0300

    std bump

--- example.ts
+++ example.ts
@@ -1,4 +1,4 @@
-import { serve } from 'https://deno.land/std@0.90.0/http/server.ts'
+import { serve } from 'https://deno.land/std@0.95.0/http/server.ts'
 import { compression } from './mod.ts'
 
 const s = serve({ port: 3000 })
