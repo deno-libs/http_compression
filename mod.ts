2b60447569bd55757f3d1580f3f82d02ee29ee06
v1rtl <pilll.pl22@gmail.com>
2021-09-22 13:31:55 +0300

    close file after reading and add tests

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 import { compress as brotli } from 'https://deno.land/x/brotli@v0.1.4/mod.ts'
 import { gzip, deflate } from 'https://deno.land/x/denoflate@1.2.1/mod.ts'
 import { Accepts } from 'https://deno.land/x/accepts@2.1.1/mod.ts'
-import { readAll } from 'https://deno.land/std@0.107.0/io/util.ts'
+import { readAll } from 'https://deno.land/std@0.108.0/io/util.ts'
 
 const funcs = {
   br: brotli,
@@ -31,7 +31,7 @@ export type CompressionOptions = {
  *
  * @example
  * ```ts
- *import { serve } from 'https://deno.land/std@0.107.0/http/server.ts'
+ *import { serve } from 'https://deno.land/std@0.108.0/http/server.ts'
  *import { compression } from 'https://deno.land/x/compression/brotli.ts'
  *
  *const s = serve({ port: 3000 })
@@ -44,62 +44,67 @@ export type CompressionOptions = {
  *}
  * ```
  */
-export const compression = (opts: CompressionOptions) => async (req: Request) => {
-  const acceptHeader = req.headers.get('Accept-Encoding')
+export const compression =
+  (opts: CompressionOptions) =>
+  async (req: Request): Promise<Response> => {
+    const acceptHeader = req.headers.get('Accept-Encoding')
 
-  const accepts = new Accepts(req.headers)
+    const accepts = new Accepts(req.headers)
 
-  const encodings = accepts.encodings()
+    const encodings = accepts.encodings()
 
-  const buf = await readAll(await Deno.open(opts.path))
+    const file = await Deno.open(opts.path)
 
-  if (!acceptHeader || acceptHeader === 'identity' || (Array.isArray(encodings) && encodings[0] === 'identity')) {
-    return new Response(buf, {
-      status: 200,
-      headers: new Headers({
-        'Content-Encoding': 'identity'
+    const buf = await readAll(file)
+    file.close()
+
+    if (!acceptHeader || acceptHeader === 'identity' || (Array.isArray(encodings) && encodings[0] === 'identity')) {
+      return new Response(buf, {
+        status: 200,
+        headers: new Headers({
+          'Content-Encoding': 'identity'
+        })
       })
-    })
-  } else if (acceptHeader === '*') {
-    const preferredAlgo = opts.compression[0]
+    } else if (acceptHeader === '*') {
+      const preferredAlgo = opts.compression[0]
 
-    const compressed = funcs[preferredAlgo](buf)
-
-    return new Response(compressed, {
-      headers: new Headers({
-        'Content-Encoding': preferredAlgo
-      }),
-      status: 200
-    })
-  } else {
-    if (Array.isArray(encodings)) {
-      let compressed: Uint8Array = buf
-      const encs: string[] = []
-
-      for (let enc of encodings.filter((x) => x !== 'identity')) {
-        if (enc === 'brotli') enc = 'br'
-
-        if (Object.keys(funcs).includes(enc as string)) {
-          compressed = funcs[enc as Compression](compressed)
-          encs.push(enc)
-        }
-      }
+      const compressed = funcs[preferredAlgo](buf)
 
       return new Response(compressed, {
         headers: new Headers({
-          'Content-Encoding': encs.join(', ')
-        })
+          'Content-Encoding': preferredAlgo
+        }),
+        status: 200
       })
     } else {
-      return Object.keys(funcs).includes(encodings as string)
-        ? new Response(funcs[encodings as Compression](buf), {
-            headers: new Headers({
-              'Content-Encoding': encodings as string
+      if (Array.isArray(encodings)) {
+        let compressed: Uint8Array = buf
+        const encs: string[] = []
+
+        for (let enc of encodings.filter((x) => x !== 'identity')) {
+          if (enc === 'brotli') enc = 'br'
+
+          if (Object.keys(funcs).includes(enc as string)) {
+            compressed = funcs[enc as Compression](compressed)
+            encs.push(enc)
+          }
+        }
+
+        return new Response(compressed, {
+          headers: new Headers({
+            'Content-Encoding': encs.join(', ')
+          })
+        })
+      } else {
+        return Object.keys(funcs).includes(encodings as string)
+          ? new Response(funcs[encodings as Compression](buf), {
+              headers: new Headers({
+                'Content-Encoding': encodings as string
+              })
             })
-          })
-        : new Response('Not Acceptable', {
-            status: 406
-          })
+          : new Response('Not Acceptable', {
+              status: 406
+            })
+      }
     }
   }
-}
---
+++ mod_test.ts
@@ -0,0 +1,27 @@
+import { superdeno } from 'https://deno.land/x/superdeno@4.5.0/mod.ts'
+import { describe, it, run } from 'https://deno.land/x/tincan@0.2.2/mod.ts'
+import { compression } from './mod.ts'
+
+describe('options', () => {
+  it('does not apply compression if "Accept-Encoding" is "identity"', async () => {
+    const request = superdeno(compression({ path: 'README.md', compression: ['gzip'] }))
+
+    await request
+      .get('/')
+      .set('Accept-Encoding', 'identity')
+      .expect(200)
+      .expect('Content-Encoding', 'identity')
+      .expect('Content-Length', '1384')
+  })
+  it('applies compression to a file', async () => {
+    const request = superdeno(compression({ path: 'README.md', compression: ['br', 'gzip', 'deflate'] }))
+
+    await request
+      .get('/')
+      .set('Accept-Encoding', 'br, gzip, deflate')
+      .expect('Content-Length', '673')
+      .expect('Content-Encoding', 'br, gzip, deflate')
+  })
+})
+
+run()
