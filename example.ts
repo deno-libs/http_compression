c29bf0a33ac10cf22372fdd3d322e773522fad47
CandL Development <lukepdev@live.com>
2021-09-30 16:57:02 -0700

    Reformat example file with deno lint

--- example.ts
+++ example.ts
@@ -1,17 +1,17 @@
-import { compression } from './mod.ts'
-import { Server } from 'https://deno.land/std@0.108.0/http/server.ts'
+import { compression } from "./mod.ts";
+import { Server } from "https://deno.land/std@0.108.0/http/server.ts";
 
 const s = new Server({
   handler: async (req) => {
     return await compression({
-      path: 'README.md',
-      compression: ['br', 'gzip', 'deflate']
-    })(req)
+      path: "README.md",
+      compression: ["br", "gzip", "deflate"],
+    })(req);
   },
-  addr: ':3000'
-})
+  addr: ":3000",
+});
 
-s.listenAndServe()
+s.listenAndServe();
 console.log(
   "Server available at http://localhost:3000 .  Set Accept-Encoding header to 'gzip', for example, to get a compressed response.",
 );
