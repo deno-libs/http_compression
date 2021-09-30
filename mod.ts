e1f98395f2c21f895ac66ff22d78a983e754d7ad
CandL Development <lukepdev@live.com>
2021-09-30 16:59:13 -0700

    Add support for in-memory compression
    Allow passing in bodyText or bodyBinary in addition to path.
    Only 1 will be allowed.
    The options type was made as a union type to allow existing code to be used unchanged so there was no breaking change in the API for end users.

--- mod.ts
+++ mod.ts
@@ -1,29 +1,50 @@
-import { compress as brotli } from 'https://deno.land/x/brotli@v0.1.4/mod.ts'
-import { gzip, deflate } from 'https://deno.land/x/denoflate@1.2.1/mod.ts'
-import { Accepts } from 'https://deno.land/x/accepts@2.1.1/mod.ts'
-import { readAll } from 'https://deno.land/std@0.108.0/io/util.ts'
+import { compress as brotli } from "https://deno.land/x/brotli@v0.1.4/mod.ts";
+import { deflate, gzip } from "https://deno.land/x/denoflate@1.2.1/mod.ts";
+import { Accepts } from "https://deno.land/x/accepts@2.1.1/mod.ts";
+import { readAll } from "https://deno.land/std@0.108.0/io/util.ts";
 
 const funcs = {
   br: brotli,
   gzip: (body: Uint8Array) => gzip(body, undefined),
-  deflate: (body: Uint8Array) => deflate(body, undefined)
-}
+  deflate: (body: Uint8Array) => deflate(body, undefined),
+};
 
 /**
  * Supported compression algorithms
  */
-type Compression = 'gzip' | 'br' | 'deflate'
+type Compression = "gzip" | "br" | "deflate";
 
-export type CompressionOptions = {
-  /**
-   * Path to file
-   */
-  path: string
-  /**
+export type CompressionOptions =
+  & {
+    /**
    * Compression algorithms (gzip, brotli, deflate). The first is used if all are accepted by the client
    */
-  compression: [Compression] | [Compression, Compression] | [Compression, Compression, Compression]
-}
+    compression: [Compression] | [Compression, Compression] | [
+      Compression,
+      Compression,
+      Compression,
+    ];
+  }
+  & (
+    | {
+      /**
+* Path to file
+  */
+      path: string;
+    }
+    | {
+      /**
+* Body as a byte array (as returned from Deno.readFile methods)
+  */
+      bodyBinary: Uint8Array;
+    }
+    | {
+      /**
+  * Body as a string (as returned from Deno.readTextFile)
+  */
+      bodyText: string;
+    }
+  );
 
 /**
  * HTTP Compression middleware.
@@ -44,67 +65,78 @@ export type CompressionOptions = {
  *}
  * ```
  */
-export const compression =
-  (opts: CompressionOptions) =>
+export const compression = (opts: CompressionOptions) =>
   async (req: Request): Promise<Response> => {
-    const acceptHeader = req.headers.get('Accept-Encoding')
+    const acceptHeader = req.headers.get("Accept-Encoding");
 
-    const accepts = new Accepts(req.headers)
+    const accepts = new Accepts(req.headers);
 
-    const encodings = accepts.encodings()
+    const encodings = accepts.encodings();
 
-    const file = await Deno.open(opts.path)
+    let buf: Uint8Array;
+    if ("bodyBinary" in opts) {
+      buf = opts.bodyBinary;
+    } else if ("bodyText" in opts) {
+      const encoder = new TextEncoder();
+      buf = encoder.encode(opts.bodyText);
+    } else if ("path" in opts) {
+      const file = await Deno.open(opts.path);
+      buf = await readAll(file);
+      file.close();
+    } else {
+      throw Error("Must specify either bodyBinary, bodyText, or path.");
+    }
 
-    const buf = await readAll(file)
-    file.close()
-
-    if (!acceptHeader || acceptHeader === 'identity' || (Array.isArray(encodings) && encodings[0] === 'identity')) {
+    if (
+      !acceptHeader || acceptHeader === "identity" ||
+      (Array.isArray(encodings) && encodings[0] === "identity")
+    ) {
       return new Response(buf, {
         status: 200,
         headers: new Headers({
-          'Content-Encoding': 'identity'
-        })
-      })
-    } else if (acceptHeader === '*') {
-      const preferredAlgo = opts.compression[0]
+          "Content-Encoding": "identity",
+        }),
+      });
+    } else if (acceptHeader === "*") {
+      const preferredAlgo = opts.compression[0];
 
-      const compressed = funcs[preferredAlgo](buf)
+      const compressed = funcs[preferredAlgo](buf);
 
       return new Response(compressed, {
         headers: new Headers({
-          'Content-Encoding': preferredAlgo
+          "Content-Encoding": preferredAlgo,
         }),
-        status: 200
-      })
+        status: 200,
+      });
     } else {
       if (Array.isArray(encodings)) {
-        let compressed: Uint8Array = buf
-        const encs: string[] = []
+        let compressed: Uint8Array = buf;
+        const encs: string[] = [];
 
-        for (let enc of encodings.filter((x) => x !== 'identity')) {
-          if (enc === 'brotli') enc = 'br'
+        for (let enc of encodings.filter((x) => x !== "identity")) {
+          if (enc === "brotli") enc = "br";
 
           if (Object.keys(funcs).includes(enc as string)) {
-            compressed = funcs[enc as Compression](compressed)
-            encs.push(enc)
+            compressed = funcs[enc as Compression](compressed);
+            encs.push(enc);
           }
         }
 
         return new Response(compressed, {
           headers: new Headers({
-            'Content-Encoding': encs.join(', ')
-          })
-        })
+            "Content-Encoding": encs.join(", "),
+          }),
+        });
       } else {
         return Object.keys(funcs).includes(encodings as string)
           ? new Response(funcs[encodings as Compression](buf), {
-              headers: new Headers({
-                'Content-Encoding': encodings as string
-              })
-            })
-          : new Response('Not Acceptable', {
-              status: 406
-            })
+            headers: new Headers({
+              "Content-Encoding": encodings as string,
+            }),
+          })
+          : new Response("Not Acceptable", {
+            status: 406,
+          });
       }
     }
-  }
+  };
