c426c4cb277065f5589bd8c5eb4e9abd8ea1a1c1
v1rtl <pilll.pl22@gmail.com>
2021-10-06 11:25:50 +0300

    use license instead of readme for tests

--- mod_test.ts
+++ mod_test.ts
@@ -4,46 +4,46 @@ import { compression } from './mod.ts'
 
 describe('options', () => {
   it('does not apply compression if "Accept-Encoding" is "identity"', async () => {
-    const request = superdeno(compression({ path: 'README.md', compression: ['gzip'] }))
+    const request = superdeno(compression({ path: 'LICENSE', compression: ['gzip'] }))
 
     await request
       .get('/')
       .set('Accept-Encoding', 'identity')
       .expect(200)
       .expect('Content-Encoding', 'identity')
-      .expect('Content-Length', '2047')
+      .expect('Content-Length', '1071')
   })
   it('applies compression to a file', async () => {
     const request = superdeno(
       compression({
-        path: 'README.md',
+        path: 'LICENSE',
         compression: ['br', 'gzip', 'deflate']
       })
     )
     await request
       .get('/')
       .set('Accept-Encoding', 'br, gzip, deflate')
-      .expect('Content-Length', '854')
+      .expect('Content-Length', '628')
       .expect('Content-Encoding', 'br, gzip, deflate')
   })
   it('applies compression to a string', async () => {
-    const bodyText = await Deno.readTextFile('README.md')
+    const bodyText = await Deno.readTextFile('LICENSE')
     const request = superdeno(compression({ bodyText, compression: ['br', 'gzip', 'deflate'] }))
 
     await request
       .get('/')
       .set('Accept-Encoding', 'br, gzip, deflate')
-      .expect('Content-Length', '854')
+      .expect('Content-Length', '628')
       .expect('Content-Encoding', 'br, gzip, deflate')
   })
   it('applies compression to a byte array', async () => {
-    const bodyBinary = await Deno.readFile('README.md')
+    const bodyBinary = await Deno.readFile('LICENSE')
     const request = superdeno(compression({ bodyBinary, compression: ['br', 'gzip', 'deflate'] }))
 
     await request
       .get('/')
       .set('Accept-Encoding', 'br, gzip, deflate')
-      .expect('Content-Length', '854')
+      .expect('Content-Length', '628')
       .expect('Content-Encoding', 'br, gzip, deflate')
   })
 })
