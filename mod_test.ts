aa8ca350547231c0a4be024ae3a75f778325226c
v1rtl <pilll.pl22@gmail.com>
2022-03-19 20:50:42 +0200

    bump deps

--- mod_test.ts
+++ mod_test.ts
@@ -1,4 +1,4 @@
-import { superdeno } from 'https://deno.land/x/superdeno@4.7.2/mod.ts'
+import { superdeno } from 'https://deno.land/x/superdeno@4.8.0/mod.ts'
 import { describe, it, run } from 'https://deno.land/x/tincan@1.0.0/mod.ts'
 import { compression } from './mod.ts'
 
