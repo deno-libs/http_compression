40fc7f7914fb02bd1589c15c0aee116009c1950d
v1rtl <pilll.pl22@gmail.com>
2023-01-05 12:43:13 +0200

    fix

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
 import { readAll } from 'https://deno.land/std@0.170.0/streams/read_all.ts'
 import { compress as brotli } from 'https://deno.land/x/brotli@0.1.7/mod.ts'
-import { Foras, gzip, deflate } from 'https://deno.land/x/foras@2.0.3/deno/mod.ts'
+import { Foras, gzip, deflate } from 'https://deno.land/x/foras@2.0.3/src/deno/mod.ts'
 import { Accepts } from 'https://deno.land/x/accepts@2.1.1/mod.ts'
 
 await Foras.initSyncBundledOnce()
