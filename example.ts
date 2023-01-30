132df391d6ac6f0ab1a8f31f89cd19f28ef676b8
v1rtl <pilll.pl22@gmail.com>
2023-01-30 12:35:56 +0200

    use deno fmt

--- example.ts
+++ example.ts
@@ -4,13 +4,13 @@ import { Server } from 'https://deno.land/std@0.175.0/http/server.ts'
 const s = new Server({
   handler: async (req) => {
     return await compression({
-      path: 'README.md'
+      path: 'README.md',
     })(req)
   },
-  port: 3000
+  port: 3000,
 })
 
 s.listenAndServe()
 console.log(
-  "Server available at http://localhost:3000. Set Accept-Encoding header to 'gzip', for example, to get a compressed response."
+  'Server available at http://localhost:3000. Set Accept-Encoding header to \'gzip\', for example, to get a compressed response.',
 )
